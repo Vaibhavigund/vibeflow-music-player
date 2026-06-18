16217ea16d197e4e871c4b501d77e707c1f9d6c0
Vaibhavi Gund <vaibhavi.gund-0324@gmail.com>
2026-06-18 21:01:49 +0530

    Completed Task 2

--- js/playlist-data.js
+++ js/playlist-data.js
@@ -14,42 +14,42 @@ const playlist = [
     {
         title: "Tum Ho Toh",
         artist: "Jubin Nautiyal",
-        cover: "assets/images/cover1.jpg",
+        cover: "assets/images/cover1.png",
         audio: "assets/music/song1.mp3",
         duration: "3:45"
     },
     {
         title: "Bairan",
         artist: "Arijit Singh",
-        cover: "assets/images/cover2.jpg",
+        cover: "assets/images/cover2.png",
         audio: "assets/music/song2.mp3",
         duration: "4:12"
     },
     {
         title: "Sajni",
         artist: "Arijit Singh",
-        cover: "assets/images/cover3.jpg",
+        cover: "assets/images/cover3.png",
         audio: "assets/music/song3.mp3",
         duration: "3:28"
     },
     {
         title: "Sanam",
         artist: "Shreya Ghoshal",
-        cover: "assets/images/cover4.jpg",
+        cover: "assets/images/cover4.png",
         audio: "assets/music/song4.mp3",
         duration: "5:03"
     },
     {
         title: "Sahiba",
         artist: "Aditya Rikhari",
-        cover: "assets/images/cover5.jpg",
+        cover: "assets/images/cover5.png",
         audio: "assets/music/song5.mp3",
         duration: "3:56"
     },
     {
         title: "Ocean Breeze",
         artist: "Coastal Vibes",
-        cover: "assets/images/cover6.jpg",
+        cover: "assets/images/cover6.png",
         audio: "assets/music/song6.mp3",
         duration: "4:31"
     }
